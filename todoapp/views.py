5d6afcecebef519565075ca8da7aa0d96995ed41
sajay <sajaykannan10@gmail.com>
2022-09-13 11:30:34 +0530

    created reg and login view

--- todoapp/views.py
+++ todoapp/views.py
@@ -6,3 +6,16 @@ from django.views.generic import View
 class SignUpView(View):
     def get(self,request,*args,**kwargs):
         return render(request,"registration.html")
+
+    def post(self,request,*args,**kwargs):
+        print(request.POST.get("firstname"))
+        print(request.POST.get("lastname"))
+        return render(request, "registration.html")
+
+class LoginView(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,"login.html")
+    def post(self,request,*args,**kwargs):
+        print(request.POST.get("username"))
+        print(request.POST.get("password"))
+        return render(request,"login.html")
